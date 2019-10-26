c29c06a700ebca89c917675675aebd603ebcd79f
VladimirV3 <virushere@icloud.com>
2019-10-26 10:16:56 +0300

    Initial Commit

---
+++ HomeWork_3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HomeWork_3
+//
+//  Created by Владимир Воронов on 10/26/19.
+//  Copyright © 2019 Vladimir Oleinikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
